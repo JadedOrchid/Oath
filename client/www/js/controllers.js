f481539469c1b799b125a1ea95105536cabd58d9
Bradley Heinz <bradley.heinz@gmail.com>
2015-04-03 17:24:14 -0700

    (refactor) Pass user input for goal details into controller

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -3,7 +3,25 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('GoalDetailCtrl', function($scope, User, $stateParams, $state){
+  $scope.details = {
+    selectedItem: null,
+    unitInput: null
+  };
+  // $scope.unitInput;
   $scope.goalType = User.goal.goalType;
+  $scope.times = [
+    "One Day",
+    "One Week",
+    "One Month",
+    "One Year"
+  ];
+  $scope.clicked = function(selectedItem, unitInput){
+    $scope.details.selectedItem = this.selectedItem;
+    $scope.details.unitInput = this.unitInput;
+    console.log("This is scope in the function", $scope);
+    console.log("This is this: ", this);
+  }
+
 })
 
 .controller('GoalCtrl', function($scope, User, $stateParams, $state) {
