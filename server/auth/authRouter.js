205eca7249f5947ee11d8371870df594282a6fb9
david-hammer <dhammer126@gmail.com>
2015-04-07 18:52:33 -0700

    (feat) hook local signup/login up with backend currently, no useful error message sent from server in event of incorrect password / duplicate email address

--- server/auth/authRouter.js
+++ server/auth/authRouter.js
@@ -8,8 +8,12 @@ module.exports = function(app, passport) {
         res.json(req.user);
       });
   //authenticate with local strategy
-  app.post('/login', passport.authenticate('local-login'))
-  app.post('/signup', passport.authenticate('local-login'))
+  app.post('/login', passport.authenticate('local-login'), function(req,res){
+        res.json(req.user);
+      });
+  app.post('/signup', passport.authenticate('local-signup'), function(req,res){
+        res.json(req.user);
+      });
 
   //connect jawbone
   app.get('/jawbone', passport.authorize('jawbone', { scope : ['basic_read','extended_read','friends_read','move_read','sleep_read','meal_read','mood_write'] }));
--- server/config/passport.js
+++ server/config/passport.js
@@ -49,10 +49,10 @@ module.exports = function(passport) {
 
                 // if no user is found, return the message
                 if (!user)
-                    return done(null, false, req.flash('loginMessage', 'No user found.'));
+                    return done(null, false); // no user found
 
                 if (!user.validPassword(password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+                    return done(null, false); // wrong password
 
                 // all is well, return user
                 else
@@ -86,7 +86,7 @@ module.exports = function(passport) {
 
                 // check to see if there's already a user with that email
                 if (existingUser) 
-                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                    return done(null, false); // email already taken
 
                 //  If we're logged in, we're connecting a new local account.
                 if(req.user) {
