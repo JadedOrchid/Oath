8ac419e591aa38056233bc976cd57fed17d7608a
david-hammer <dhammer126@gmail.com>
2015-04-09 16:49:22 -0700

    change config to use nconf

---
+++ server/config/auth.js
@@ -0,0 +1,25 @@
+var nconf = require('nconf'); 
+
+nconf.file('settings.json'); //add secret file
+nconf.env(); //use env variables if no file found
+
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : nconf.get('facebookKey'),
+        'clientSecret'  : nconf.get('facebookSecret'),
+        'callbackURL'   : nconf.get('facebookCallback') 
+    },
+    'twitterAuth' : {
+        'consumerKey'       : nconf.get('twitterKey'),
+        'consumerSecret'    : nconf.get('twitterSecret'),
+        'callbackURL'       : nconf.get('twitterCallback')
+    },
+    'jawboneAuth' : {
+       'clientID'         :  nconf.get('jawboneKey'),
+       'clientSecret'     :  nconf.get('jawboneSecret'),
+       'authorizationURL' : 'https://jawbone.com/auth/oauth2/auth',
+       'tokenURL'         : 'https://jawbone.com/auth/oauth2/token',
+       'callbackURL'      : nconf.get('jawboneCallback')
+    }
+};
