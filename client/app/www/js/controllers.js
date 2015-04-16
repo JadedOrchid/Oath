a085d2a178e10767937c125307e7babae3e40e45
JasonLy <jason.ly@gmail.com>
2015-04-16 16:03:41 -0700

    (design) Built time graph on the progress page

--- client/app/www/js/controllers.js
+++ client/app/www/js/controllers.js
@@ -124,13 +124,24 @@ angular.module('starter.controllers', [])
         value: + goal.target - goal.progress,
         color:'#F7464A',
         highlight: '#FF5A5E',
-        label: 'Red'
+        // label: 'Red'
       },
       {
         value: goal.progress,
         color: '#46BFBD',
         highlight: '#5AD3D1',
-        label: 'Green'
+        // label: 'Current'
+      } ];
+
+    datum.timeData = [{
+        value: + goal.target - goal.progress,
+        color:'green',
+        highlight: 'green',
+      },
+      {
+        value: goal.progress,
+        color: 'blue',
+        highlight: 'blue',
       } ];
 
     return datum;
@@ -139,35 +150,35 @@ angular.module('starter.controllers', [])
     // Chart.js Options
    $scope.options =  {
 
-      // // Sets the chart to be responsive
-      // responsive: true,
+      // Sets the chart to be responsive
+      responsive: true,
 
-      // //Boolean - Whether we should show a stroke on each segment
-      // segmentShowStroke : true,
+      //Boolean - Whether we should show a stroke on each segment
+      segmentShowStroke : true,
 
-      // //String - The colour of each segment stroke
-      // segmentStrokeColor : '#fff',
+      //String - The colour of each segment stroke
+      segmentStrokeColor : '#fff',
 
-      // //Number - The width of each segment stroke
-      // segmentStrokeWidth : 2,
+      //Number - The width of each segment stroke
+      segmentStrokeWidth : 2,
 
-      // //Number - The percentage of the chart that we cut out of the middle
-      // percentageInnerCutout : 50, // This is 0 for Pie charts
+      //Number - The percentage of the chart that we cut out of the middle
+      percentageInnerCutout : 50, // This is 0 for Pie charts
 
-      // //Number - Amount of animation steps
-      // animationSteps : 100,
+      //Number - Amount of animation steps
+      animationSteps : 100,
 
-      // //String - Animation easing effect
-      // animationEasing : 'easeOutBounce',
+      //String - Animation easing effect
+      animationEasing : 'easeOutBounce',
 
-      // //Boolean - Whether we animate the rotation of the Doughnut
-      // animateRotate : true,
+      //Boolean - Whether we animate the rotation of the Doughnut
+      animateRotate : true,
 
-      // //Boolean - Whether we animate scaling the Doughnut from the centre
-      // animateScale : false,
+      //Boolean - Whether we animate scaling the Doughnut from the centre
+      animateScale : false,
 
-      // //String - A legend template
-      // legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+      //String - A legend template
+      legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
     };
 }])
 
