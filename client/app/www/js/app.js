8bc5c5a87560a3c6d0e80f50a4f2608d4e14b906
david-hammer <dhammer126@gmail.com>
2015-04-18 15:22:00 -0700

    (bugfix) redirect user back to login if they try to go to an endpoint without a goal in progress.

--- client/app/www/js/app.js
+++ client/app/www/js/app.js
@@ -1,5 +1,3 @@
-// Ionic Starter App
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
@@ -22,32 +20,8 @@ angular.module('starter', ['ionic', 'ngCookies', 'starter.controllers', 'starter
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-  // .state('tab', {
-  //   url: "/tab",
-  //   abstract: true,
-  //   templateUrl: "templates/tabs.html"
-  // })
-
-  // // Each tab has its own nav history stack:
-  // .state('tab.progress', {
-  //   url: '/progress',
-  //   views: {
-  //     'tab-progress': {
-  //       templateUrl: 'templates/tab-progress.html',
-  //       controller: 'ProgressCtrl'
-  //     }
-  //   }
-  // })
-
-  .state('/', {
+  .state('root', {
     url: '/',
     cache: false,
     controller: 'SessionCtrl',
@@ -70,6 +44,7 @@ angular.module('starter', ['ionic', 'ngCookies', 'starter.controllers', 'starter
   })
 
   .state('goaltype', {
+    cache: false,
     url: '/goaltype',
     templateUrl: 'templates/goaltype.html',
     controller: 'GoalCtrl',
@@ -141,7 +116,6 @@ angular.module('starter', ['ionic', 'ngCookies', 'starter.controllers', 'starter
     templateUrl: 'templates/tab-settings.html'
   });
 
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/');
 
 });
